5dfd84829f637dd23f6d05ff52c2e740cb9346e8
Muhammad Usman <prousman741@gmail.com>
2022-01-03 21:01:33 +0500

    Header - Implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Header from './layout/Header'
 import Navigation from './layout/Navigation'
 
 const App = () => {
   return (
     <>
       <Navigation />
+      <Header />
     </>
   )
 }
---
+++ src/layout/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1 className='header-heading'>ZITKANI</h1>
+            <p className='header-paragraph'>UKRAINE</p>
+        </div>
+    )
+}
+
+export default Header
