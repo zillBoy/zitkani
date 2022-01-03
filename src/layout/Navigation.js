a4df6123687d39ac9cadc81c69b839304a171502
Muhammad Usman <prousman741@gmail.com>
2022-01-03 20:48:01 +0500

    Navigation - Implemented

--- src/layout/Navigation.js
+++ src/layout/Navigation.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import TextChevron from '../components/TextChevron'
 import NavigationHeader from './NavigationHeader'
 
 const Navigation = () => {
     return (
         <div>
             <NavigationHeader />
+            <div className='navigation-maincontainer'>
+                <div className='navigation-container'>
+                    <TextChevron text='Shop' />
+                    <TextChevron text='Collections' />
+                    <p>About</p>
+                    <p>Delivery&Payment</p>
+                    <p>Contact</p>
+                </div>
+            </div>
         </div>
     )
 }
